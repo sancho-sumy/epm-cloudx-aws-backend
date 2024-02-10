a5a96c3d7ad4e2e4e1fc13b225e751ce6a151f0b
sancho-sumy <www.sancho@gmail.com>
2024-02-10 20:18:20 +0200

    feat: add swagger

--- product-service/src/functions/products/index.ts
+++ product-service/src/functions/products/index.ts
@@ -1,25 +1,31 @@
-import { handlerPath } from "@libs/handler-resolver";
+import { handlerPath } from '@libs/handler-resolver';
 
 export const getProductsList = {
-  handler: `${handlerPath(__dirname)}/getProductsList.main`,
-  events: [
-    {
-      httpApi: {
-        method: "get",
-        path: "/products",
-      },
-    },
-  ],
+	handler: `${handlerPath(__dirname)}/getProductsList.main`,
+	events: [
+		{
+			httpApi: {
+				method: 'get',
+				path: '/products',
+				summary: 'All products',
+				description: 'Return a list of products',
+				swaggerTags: ['Products']
+			}
+		}
+	]
 };
 
 export const getProductsById = {
-  handler: `${handlerPath(__dirname)}/getProductsById.main`,
-  events: [
-    {
-      httpApi: {
-        method: "get",
-        path: "/products/{productId}",
-      },
-    },
-  ],
+	handler: `${handlerPath(__dirname)}/getProductsById.main`,
+	events: [
+		{
+			httpApi: {
+				method: 'get',
+				path: '/products/{productId}',
+				summary: 'Product by ID',
+				description: 'Return a product by ID',
+				swaggerTags: ['Products']
+			}
+		}
+	]
 };
